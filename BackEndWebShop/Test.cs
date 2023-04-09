7842a2c1f8df4f9bfe51be8f66357792f6bb03eb
Minhdeptrai09 <chaungocminh202@gocminh202gmail.com>
2023-04-09 22:24:44 +0700

    add

--- BackEndWebShop/Test.cs
+++ BackEndWebShop/Test.cs
@@ -1,6 +1,12 @@
-﻿namespace BackEndWebShop
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackEndWebShop
 {
     public class Test
     {
+        public IActionResult index()
+        {
+            return OkObjectResult;
+        }
     }
 }
